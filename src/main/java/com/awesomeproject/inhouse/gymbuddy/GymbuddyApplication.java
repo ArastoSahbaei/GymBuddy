61cf1a1605faf4a3c1596a700191ad1d8ccc4a4b
Marcin Retek <marcinretek@hotmail.com>
2019-04-04 21:43:45 +0200

    Initial commit

---
+++ src/main/java/com/awesomeproject/inhouse/gymbuddy/GymbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.awesomeproject.inhouse.gymbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymbuddyApplication.class, args);
+	}
+
+}
